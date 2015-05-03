2f85cf38c6f38a183b004066e775b7d7e23d4a91
todd-leshan <todd.leshan@gmail.com>
2015-05-03 23:13:42 +1000

    product update

--- app/views/content/manageProducts.php
+++ app/views/content/manageProducts.php
@@ -36,15 +36,36 @@ foreach ($products as $product) :
 	$status       = $product->getStatus();
 ?>	
 	<tr>
-		<td><?php echo $name; ?></td>
-		<td><?php echo $price; ?></td>
-		<td  class="productDesc"><?php echo $description; ?></td>
-		<td><img src="/sportsgear/public/images/product/<?php echo $gearTypeName.'/'.$photoName; ?>" alt="<?php echo $photoAlt;?>"></td>
-		<td><?php echo $brandName; ?></td>
-		<td><?php echo $gearTypeName; ?></td>
-		<td><?php echo $sportTypeName; ?></td>
-		<td><?php echo $status; ?></td>
-		<td><a href="<?php echo ROOT.'staff/editProduct/'.$productID; ?>">Edit</a></td>
+		<form method="post">
+			<td><?php echo $name; ?></td>
+			<td>
+				<input type="number" name="change-price" id="change-price" required min="0" step="0.01" value="<?php echo $price; ?>">
+			</td>
+			<td  class="productDesc">
+				<textarea name="change-description" id="change-description" required><?php echo $description; ?></textarea>
+			</td>
+			<td><img src="/sportsgear/public/images/product/<?php echo $gearTypeName.'/'.$photoName; ?>" alt="<?php echo $photoAlt;?>"></td>
+			<td><?php echo $brandName; ?></td>
+			<td><?php echo $gearTypeName; ?></td>
+			<td><?php echo $sportTypeName; ?></td>
+			<td>
+			
+				<label for="status-1">
+					<input type="radio" name="change-status" id="status-1" value="<?php echo $status;?>" <?php if($status == 1): echo 'checked'; endif;?>>
+					active	
+				</label>
+				<br>
+				<label for="status-0">
+					<input type="radio" name="change-status" id="status-0" value="0" <?php if($status == 0): echo 'checked'; endif;?>>
+					inactive	
+				</label>
+			</td>
+			<td>
+				<button type="submit" name="change-update">Update</button>
+				<br>
+				<button type="submit" name="change-delete">Delete</button>
+			</td>
+		</form>
 	</tr>
 <?php
 endforeach;
